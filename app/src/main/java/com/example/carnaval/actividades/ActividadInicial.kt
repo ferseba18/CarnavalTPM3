9a9ef3d47bb3a0f0a0d913c50dbbc653dd460894
fabian <zaratefabian@hotmail.com>
2022-05-19 22:56:12 -0300

    Implementacion de ViewBinding

--- app/src/main/java/com/example/carnaval/actividades/ActividadInicial.kt
+++ app/src/main/java/com/example/carnaval/actividades/ActividadInicial.kt
@@ -4,11 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.carnaval.R
+import com.example.carnaval.databinding.ActivityActividadInicialBinding
 
 class ActividadInicial : AppCompatActivity() {
+
+    private lateinit var binding: ActivityActividadInicialBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_actividad_inicial)
+        binding =ActivityActividadInicialBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonMap.setOnClickListener { verUbicacion() }
+
 
 
     }
@@ -17,5 +25,8 @@ class ActividadInicial : AppCompatActivity() {
         val intent = Intent(this, ActividadStand::class.java)
         startActivity(intent)
     }
-
+    private fun verUbicacion() {
+        val intent = Intent(this, ActividadUbicacion::class.java)
+        startActivity(intent)
+    }
 }
