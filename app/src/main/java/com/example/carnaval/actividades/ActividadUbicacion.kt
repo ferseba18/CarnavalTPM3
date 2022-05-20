35063774c7709647dd3c2ad5eb65ebced1643c8e
fabian <zaratefabian@hotmail.com>
2022-05-20 20:01:24 -0300

    Visualizacion de map en el ActivityUbicacion

--- app/src/main/java/com/example/carnaval/actividades/ActividadUbicacion.kt
+++ app/src/main/java/com/example/carnaval/actividades/ActividadUbicacion.kt
@@ -3,13 +3,46 @@ package com.example.carnaval.actividades
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.carnaval.R
+import com.example.carnaval.databinding.ActivityActividadUbicacionBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class ActividadUbicacion : AppCompatActivity() {
+class ActividadUbicacion : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var binding: ActivityActividadUbicacionBinding
+    private lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_actividad_ubicacion)
+        binding = ActivityActividadUbicacionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        createFragment()
+
     }
+
+    private fun createFragment(){
+        val mapFragment :SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        map = googleMap
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+    }
+
+
+
     private fun verStand() {
         val intent = Intent(this, ActividadStand::class.java)
         startActivity(intent)
