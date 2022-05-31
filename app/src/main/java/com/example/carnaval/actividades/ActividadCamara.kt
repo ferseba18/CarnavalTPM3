3f5b0421620ee8bc2ca00d07cbf1dbfc1ed14539
fabian <zaratefabian@hotmail.com>
2022-05-30 21:03:39 -0300

    Cambio de icono

--- app/src/main/java/com/example/carnaval/actividades/ActividadCamara.kt
+++ app/src/main/java/com/example/carnaval/actividades/ActividadCamara.kt
@@ -23,25 +23,59 @@ import java.util.concurrent.ExecutorService
 import android.provider.MediaStore
 
 import android.content.ContentValues
+import android.media.MediaScannerConnection
+import android.net.Uri
 import android.os.Build
+import android.util.DisplayMetrics
+import android.webkit.MimeTypeMap
+import androidx.core.net.toFile
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.carnaval.databinding.ActivityActividadCamaraBinding
+import java.io.File
 
 typealias LumaListener = (luma: Double) -> Unit
 
 class ActividadCamara : AppCompatActivity() {
-    private lateinit var viewBinding: ActivityActividadCamaraBinding
+    private lateinit var binding: ActivityActividadCamaraBinding
+
+    private lateinit var broadcastManager: LocalBroadcastManager
+
+    private var displayId: Int = -1
+    private var preview: Preview? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+
+    private var lensFacing: Int = CameraSelector.LENS_FACING_BACK
+    private var cameraProvider: ProcessCameraProvider? = null
+
+    var cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
     private var imageCapture: ImageCapture? = null
-
-    private var videoCapture: VideoCapture<Recorder>? = null
-    private var recording: Recording? = null
-
+    private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBinding = ActivityActividadCamaraBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root)
+        binding = ActivityActividadCamaraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        binding.cameraCaptureButton.setOnClickListener {
+            takePhoto()
+        }
+
+        binding.cameraSwitchButton.setOnClickListener {
+            lensFacing = if (CameraSelector.LENS_FACING_FRONT == lensFacing) {
+                CameraSelector.LENS_FACING_BACK
+            } else {
+                CameraSelector.LENS_FACING_FRONT
+            }
+            // Se realiza nuevamente la vinculacion
+            bindCameraUseCases()
+        }
+
 
         // Request camera permissions
         if (allPermissionsGranted()) {
@@ -51,23 +85,140 @@ class ActividadCamara : AppCompatActivity() {
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
-        // Set up the listeners for take photo and video capture buttons
-        viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
-        viewBinding.videoCaptureButton.setOnClickListener { captureVideo() }
 
-        cameraExecutor = Executors.newSingleThreadExecutor()
+
     }
 
-    private fun takePhoto() {}
+    private fun takePhoto() {
+        // Se crea el archivo de salida
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg")
 
-    private fun captureVideo() {}
+        // Crear archivo de salida incluyendo metadatos.
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
 
-    private fun startCamera() {}
+        //Capturar la imagen actual de la camara
+        imageCapture?.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Error al realizar la captura de la foto: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+
+                    Toast.makeText(baseContext, "IMG guardada ${savedUri}", Toast.LENGTH_SHORT).show()
+
+
+
+                    // Utilizamos [MediaScannerConnection] para escanear los medios de la galeria
+                    val mimeType = MimeTypeMap.getSingleton()
+                        .getMimeTypeFromExtension(savedUri.toFile().extension)
+                    MediaScannerConnection.scanFile(
+                        baseContext,
+                        arrayOf(savedUri.toFile().absolutePath),
+                        arrayOf(mimeType)
+                    ) { _, uri ->
+                        Log.d(TAG, "Imagen capturada fue guardada: $uri")
+                    }
+                }
+            })
+
+    }
+
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+             cameraProvider = cameraProviderFuture.get()
+
+            // Cambio entre camaras dependiendo de la disponibilidad
+            lensFacing = when {
+                hasBackCamera() -> CameraSelector.LENS_FACING_BACK
+                hasFrontCamera() -> CameraSelector.LENS_FACING_FRONT
+                else -> throw IllegalStateException("No existen dispositivo de captura disponibles...")
+            }
+
+            // Habilitar o deshabilitar boton de cambio de camaras
+            updateCameraSwitchButton()
+
+            bindCameraUseCases()
+        }, ContextCompat.getMainExecutor(this))
+
+    }
+
+    private fun bindCameraUseCases() {
+
+
+
+        // CameraProvider
+        val cameraProvider = cameraProvider
+            ?: throw IllegalStateException("Error al iniciar la camara.")
+
+        // CameraSelector
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+        // Preview
+        preview = Preview.Builder()
+            .build()
+
+        // ImageCapture
+        imageCapture = ImageCapture.Builder()
+            .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+            .build()
+
+
+
+        // Desvincular para poder usar de nuevo
+        cameraProvider.unbindAll()
+
+        try {
+            //Vincular la camara a "use cases"
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+
+            // Generar vista previa de la camara
+            preview?.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+        } catch (exc: Exception) {
+            Log.e(TAG, "Use case fallo", exc)
+        }
+    }
+
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, "ejcamerax").apply { mkdirs() } }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
             baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
+
+    private fun updateCameraSwitchButton() {
+        val switchCamerasButton = binding.cameraSwitchButton
+        try {
+            switchCamerasButton.isEnabled = hasBackCamera() && hasFrontCamera()
+        } catch (exception: CameraInfoUnavailableException) {
+            switchCamerasButton.isEnabled = false
+        }
+    }
+
+    /** Devuelte true si el dispositivo cuenta con camara trasera, caso contrario devuelve false */
+    private fun hasBackCamera(): Boolean {
+        return cameraProvider?.hasCamera(CameraSelector.DEFAULT_BACK_CAMERA) ?: false
+    }
+
+    /** Devuelte true si el dispositivo cuenta con camara frontal, caso contrario devuelve false */
+    private fun hasFrontCamera(): Boolean {
+        return cameraProvider?.hasCamera(CameraSelector.DEFAULT_FRONT_CAMERA) ?: false
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
@@ -76,8 +227,9 @@ class ActividadCamara : AppCompatActivity() {
 
     companion object {
         private const val TAG = "CameraXApp"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val FILENAME = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
+        private const val PHOTO_EXTENSION = ".jpg"
         private val REQUIRED_PERMISSIONS =
             mutableListOf (
                 Manifest.permission.CAMERA,
@@ -97,7 +249,7 @@ class ActividadCamara : AppCompatActivity() {
                 startCamera()
             } else {
                 Toast.makeText(this,
-                    "Permissions not granted by the user.",
+                    "Para activar la Camara ve a ajustes y acepta los permisos",
                     Toast.LENGTH_SHORT).show()
                 finish()
             }
@@ -105,4 +257,8 @@ class ActividadCamara : AppCompatActivity() {
         }
     }
 
+    private fun createFile(baseFolder: File, format: String, extension: String) =
+        File(baseFolder, SimpleDateFormat(format, Locale.US)
+            .format(System.currentTimeMillis()) + extension)
 }
+
