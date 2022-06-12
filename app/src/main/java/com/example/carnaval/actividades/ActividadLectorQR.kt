95a6e705b8f115fcad2bf063f53fd869fb0a2396
fabian <zaratefabian@hotmail.com>
2022-06-11 21:55:36 -0300

    Funcionalidad exitosa de QR

--- app/src/main/java/com/example/carnaval/actividades/ActividadLectorQR.kt
+++ app/src/main/java/com/example/carnaval/actividades/ActividadLectorQR.kt
@@ -1,7 +1,9 @@
 package com.example.carnaval.actividades
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.carnaval.R
 import com.example.carnaval.databinding.ActivityActividadLectorQrBinding
 import com.google.zxing.integration.android.IntentIntegrator
@@ -19,6 +21,26 @@ class ActividadLectorQR : AppCompatActivity() {
     }
 
     private fun initScanner() {
-        IntentIntegrator(this).initiateScan()
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("Sigue aprendiendo en CursoKotlin.com")
+        integrator.setTorchEnabled(true)
+        integrator.setBeepEnabled(true)
+        integrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        var btnScann = binding.button2
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_LONG).show()
+            } else {
+                btnScann.text = result.contents
+                //Toast.makeText(this, "El valor escaneado es: " + result.contents, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
