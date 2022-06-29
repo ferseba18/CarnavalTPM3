94d1c86a1f0cf22f19b1f5d49e25c3a0c8f4b1d4
ferseba18 <fer_seba_18@yahoo.com.ar>
2022-06-29 06:13:55 -0300

    Update activity_premios.kt

--- app/src/main/java/com/example/carnaval/actividades/activity_premios.kt
+++ app/src/main/java/com/example/carnaval/actividades/activity_premios.kt
@@ -1,86 +1,99 @@
 package com.example.carnaval.actividades
 
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatActivity
 import com.example.carnaval.R
+import com.google.android.gms.maps.model.Circle
+import java.lang.Math.sqrt
+import java.util.*
+import kotlin.collections.ArrayList
 
 class activity_premios : AppCompatActivity(), SensorEventListener {
- private lateinit var sensorManager: SensorManager
- private var Intentos: Boolean = false
- private lateinit var Circle: TextView
- private var Giro: Boolean = false
- val numeros: MutableList<Int> = ArrayList()
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
+    private lateinit var numeros: ArrayList<Int>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_premios)
-        Circle=findViewById(R.id.Circulo)
-    }
-    fun addnumeros(){
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        Objects.requireNonNull(sensorManager)!!.registerListener(sensorListener, sensorManager!!
+            .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+        acceleration = 10f
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
         numeros.clear()
-        numeros.add(1);
-        numeros.add(2);
-        numeros.add(3);
-        numeros.add(4);
-        numeros.add(5);
-        numeros.add(6);
-        numeros.add(7);
-        numeros.add(8);
-        numeros.add(9);
-        numeros.add(10);
+        numeros.add(1)
+        numeros.add(2)
+        numeros.add(3)
+        numeros.add(4)
+        numeros.add(5)
+        numeros.add(6)
+        numeros.add(7)
+        numeros.add(8)
+        numeros.add(9)
+        numeros.add(10)
     }
-    fun SetupSensor(){
-        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also{
-            sensorManager.registerListener(this,it,
-                SensorManager.SENSOR_DELAY_FASTEST,
-                SensorManager.SENSOR_DELAY_FASTEST)
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            var intento= numeros.random()
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+            if (acceleration > 12) {
+                if (intento==3){
+                    Toast.makeText(applicationContext, "Ganaste una Bicicleta", Toast.LENGTH_SHORT).show()
+                }
+                else
+                    if(intento==7){
+                        Toast.makeText(applicationContext, "Ganaste un set pirata", Toast.LENGTH_SHORT).show()
+                    }
+                else
+                    if(intento==10){
+                        Toast.makeText(applicationContext, "Ganaste una mochila", Toast.LENGTH_SHORT).show()
+                    }
+                else{
+                        Toast.makeText(applicationContext, "Segui Participando", Toast.LENGTH_SHORT).show()
+                }
+                }
+            }
+
+        override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+            TODO("Not yet implemented")
         }
     }
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        if(event?.sensor?.type== Sensor.TYPE_ACCELEROMETER){
-            val sides= event.values[0]
-            val upDown= event.values[1]
-            if(upDown.toInt() == 0 && sides.toInt()== 0){
-                Toast.makeText(this, "Debes Mover el Celular para realizar tu intento diario a un premio!", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                Giro=true
-            }
-        }
+    override fun onSensorChanged(p0: SensorEvent?) {
+        TODO("Not yet implemented")
     }
+
+
+
+    override fun onResume() {
+        sensorManager?.registerListener(sensorListener, sensorManager!!.getDefaultSensor(
+            Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
+        )
+        super.onResume()
+    }
+    override fun onPause() {
+        sensorManager!!.unregisterListener(sensorListener)
+        super.onPause()
+    }
+
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
         TODO("Not yet implemented")
-    }
-    fun JugarPremios(){
-        if(Giro!=false){
-            if(Intentos!=true){
-             var tuIntento=numeros.random()
-                if(tuIntento==3){
-                    Toast.makeText(this, "Felicidades! Has Ganado la Bicicleta!", Toast.LENGTH_SHORT).show()
-                }
-                else if(tuIntento==7){
-                    Toast.makeText(this, "Felicidades! Has Ganado La Mochila!", Toast.LENGTH_SHORT).show()
-                }
-                else if (tuIntento==10){
-                    Toast.makeText(this, "Felicidades! Ganaste el Set Pirata!", Toast.LENGTH_SHORT).show()
-                }
-                else{
-                    Toast.makeText(this, "No has ganado nada, pero Segui Participando!", Toast.LENGTH_SHORT).show()
-                }
-            }
-            else{
-                Toast.makeText(this, "Ya realizo el intento diario de los premios", Toast.LENGTH_SHORT).show()
-            }
-        }
-
     }
 }
